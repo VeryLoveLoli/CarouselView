0cd06011534824b15c4ee42cfbd4aca27c75c291
VeryLoveLoli <chuancan@icloud.com>
2022-04-13 15:53:23 +0800

    修改 网络库分支

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://gitee.com/cchsora/Network", .branch("2.0")),
+        .package(url: "https://gitee.com/cchsora/Network", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
